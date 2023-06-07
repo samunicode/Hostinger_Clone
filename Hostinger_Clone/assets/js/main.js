c6d4f565fc793c9a8fb739d50e56d44cde92ff8a
Sameer Chauhan <91241984+samunicode@users.noreply.github.com>
2023-06-07 10:07:00 +0530

    Add files via upload

---
+++ Hostinger_Clone/assets/js/main.js
@@ -0,0 +1,13 @@
+function updateTimer() {
+    var currentDate = new Date();
+    var hours = String(currentDate.getHours()).padStart(2, '0');
+    var minutes = String(currentDate.getMinutes()).padStart(2, '0');
+    var seconds = String(currentDate.getSeconds()).padStart(2, '0');
+    
+    document.getElementById('hours').textContent = hours;
+    document.getElementById('minutes').textContent = minutes;
+    document.getElementById('seconds').textContent = seconds;
+  }
+
+  // Update the timer every second
+  setInterval(updateTimer, 1000);
